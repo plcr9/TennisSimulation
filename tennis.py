1d4b9851b839dcb1d795cb23cdbe8613195cc561
Paul <118978940+plcr9@users.noreply.github.com>
2023-07-15 13:33:40 +0100

    Update tennis.py

--- tennis.py
+++ tennis.py
@@ -1,1 +1,16 @@
+class Player:
+  def __init__(self, name="", ranking_points=0):
+    self.name = name
+    self.ranking_points = ranking_points
 
+  def update_ranking_points(self, points_change):
+    self.ranking_points += points_change
+
+  def __str__(self):
+    return self.name
+
+  def __repr__(self):
+    return (
+      f"Player(name='{self.name}', "
+      f"ranking_points={self.ranking_points))"
+    )
