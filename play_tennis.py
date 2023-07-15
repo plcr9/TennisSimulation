562db5ff5b1e16368cdcac980684a0617f0c720f
Paul <118978940+plcr9@users.noreply.github.com>
2023-07-15 18:45:25 +0100

    Update play_tennis.py

--- play_tennis.py
+++ play_tennis.py
@@ -1,9 +1,24 @@
 from tennis import Player, Match
 
-agassi = Player("Andre Agassi", 2000)
-sampras = Player("Pete Sampras", 1200)
+n_simulations = 100
 
-test_match = Match(agassi, sampras)
+agassi = Player("Andre Agassi", 2005)
+sampras = Player("Pete Sampras", 2000)
 
-test_match.simulate_match()
-test_match.play_match()
+winners = {agassi: 0, sampras: 0}
+n_sets = {3: 0, 4: 0, 5: 0}
+
+for _ in range(n_simulations):
+  match = Match(agassi, sampras)
+  match.simulate_match()
+  match.suppress_output()
+  match.play_match()
+
+  print(match)
+
+  winners[match.winner] += 1
+  n_sets[len(match.sets)] += 1
+
+print(winners)
+print(n_sets)
+
